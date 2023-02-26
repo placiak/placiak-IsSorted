d78cee07ba592e154c042c45724667472874c9b9
placiak <gerlach.tatras@gmail.com>
2023-02-26 04:25:01 +0000

    Completed IsSorted Challenge

--- src/main/java/IsSorted.java
+++ src/main/java/IsSorted.java
@@ -7,6 +7,23 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
+
+        for(int i = 0; i<arr.length-1; i++){
+
+            if(arr[i]<arr[i+1]){
+
+                if(i == arr.length-2){
+
+                    return true;
+                }
+                //continue;
+            }else{
+                break;
+            }
+
+
+        }
+
         return false;
     }
 }
